30f50ced7e57c69b75761304c1fd0fb1f23735df
Sutharshan1993 <113273184+Sutharshan1993@users.noreply.github.com>
2025-01-11 15:13:16 +0530

    Initial Commit

---
+++ JavaKata/src/test/java/org/java/kata/list/JavaKataApplicationTests.java
@@ -0,0 +1,13 @@
+package org.java.kata.list;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaKataApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
